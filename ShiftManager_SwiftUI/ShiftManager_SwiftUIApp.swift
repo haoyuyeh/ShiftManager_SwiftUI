e75639b28e9915059fa5618055b1f62610284577
haoyuyeh <haoyuyeh@gmail.com>
2022-06-30 08:59:49 +1000

    Initial Commit

---
+++ ShiftManager_SwiftUI/ShiftManager_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftManager_SwiftUIApp.swift
+//  ShiftManager_SwiftUI
+//
+//  Created by Hao Yu Yeh on 2022/6/30.
+//
+
+import SwiftUI
+
+@main
+struct ShiftManager_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
