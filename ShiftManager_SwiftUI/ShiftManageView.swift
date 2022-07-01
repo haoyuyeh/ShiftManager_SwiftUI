28da5898955daf39634c15d124732901dadc4e60
haoyuyeh <haoyuyeh@gmail.com>
2022-07-01 21:48:30 +1000

    the beginning  of shift manage view's layout

--- ShiftManager_SwiftUI/ShiftManageView.swift
+++ ShiftManager_SwiftUI/ShiftManageView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct ShiftManageView: View {
     var body: some View {
-        Text("Hello, ShiftManageView!")
+        GeometryReader{ geo in
+            
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("Shifts Arrange")
+                        .bold()
+                        .font(.largeTitle)
+                    Spacer()
+                }
+                .padding(.bottom, 10)
+                
+                HStack{
+                    Spacer()
+                    Text("Mon (20/6)")
+                        .frame(maxWidth: geo.size.width*0.4)
+                    Text("Hours")
+                        .frame(maxWidth: geo.size.width*0.2)
+                }
+                .padding()
+                .border(.black)
+                
+                staffRow()
+            }
+            
+            
+        }
+    }
+}
+
+struct staffRow: View {
+    @State var jobSelection = ""
+    
+    var body: some View {
+        GeometryReader{ geo in
+            HStack{
+                Spacer()
+                Text("Angus")
+                    .frame(maxWidth: geo.size.width*0.2)
+               
+                .frame(maxWidth: geo.size.width*0.2)
+                Text("10")
+                    .frame(maxWidth: geo.size.width*0.2)
+            }
+            .border(.black)
+        }
     }
 }
 
