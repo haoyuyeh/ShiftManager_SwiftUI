f376eff7735287a962cabe4bc603eec60942ec4c
haoyuyeh <haoyuyeh@gmail.com>
2022-07-03 09:25:08 +1000

    updated the layout of shift manage view

--- ShiftManager_SwiftUI/ShiftManageView.swift
+++ ShiftManager_SwiftUI/ShiftManageView.swift
@@ -11,49 +11,97 @@ struct ShiftManageView: View {
     var body: some View {
         GeometryReader{ geo in
             
-            VStack{
-                HStack{
-                    Spacer()
-                    Text("Shifts Arrange")
-                        .bold()
-                        .font(.largeTitle)
-                    Spacer()
+            ScrollView{
+                VStack {
+                    HStack{
+                        Spacer()
+                        Text("Shifts Arrange")
+                            .bold()
+                            .font(.largeTitle)
+                        Spacer()
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack{
+                        Spacer()
+                        Text("Mon(20/6)")
+                            .frame(maxWidth: geo.size.width*0.4)
+                            .border(.red)
+                        Text("Hours")
+                            .frame(maxWidth: geo.size.width*0.2)
+                            .border(.red)
+                    }
+                    .padding()
+                    .border(.black)
+                    Group{
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                        staffRow()
+                            .padding(.bottom,10)
+                    }
+                    
                 }
-                .padding(.bottom, 10)
-                
-                HStack{
-                    Spacer()
-                    Text("Mon (20/6)")
-                        .frame(maxWidth: geo.size.width*0.4)
-                    Text("Hours")
-                        .frame(maxWidth: geo.size.width*0.2)
-                }
-                .padding()
-                .border(.black)
-                
-                staffRow()
             }
-            
-            
         }
     }
 }
 
 struct staffRow: View {
-    @State var jobSelection = ""
+    @State var jobSelection = 0
+    private var jobs = ["roll", "box1", "nigiri"]
+    @State var shiftSelection = 0
+    private var shifts = ["open", "general", "close"]
     
     var body: some View {
         GeometryReader{ geo in
-            HStack{
-                Spacer()
+            HStack(alignment: .center){
+                    Spacer()
                 Text("Angus")
                     .frame(maxWidth: geo.size.width*0.2)
-               
-                .frame(maxWidth: geo.size.width*0.2)
+                
+                
+                HStack(alignment: .center){
+                        Picker(selection: $jobSelection, label: Text("job")) {
+                            ForEach(0..<jobs.count, id: \.self) {
+                                Text(self.jobs[$0])
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .frame(maxWidth: .infinity)
+                        
+                        Picker(selection: $shiftSelection, label: Text("shift")) {
+                            ForEach(0..<shifts.count, id: \.self) {
+                                Text(self.shifts[$0])
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: geo.size.width*4)
+//                    .border(.green)
+                
                 Text("10")
                     .frame(maxWidth: geo.size.width*0.2)
+//                    .border(.green)
             }
-            .border(.black)
+//            .border(.black)
+            
         }
     }
 }
